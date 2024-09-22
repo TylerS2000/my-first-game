a493874d0263dc9984445ebccec83d157053a83e
TylerS2000 <tylerschulten@gmail.com>
2024-09-22 17:16:33 -0400

    hides player on load screen

--- player.gd
+++ player.gd
@@ -4,6 +4,7 @@ signal hit
 var screen_size
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	hide()
 	screen_size = get_viewport_rect()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
